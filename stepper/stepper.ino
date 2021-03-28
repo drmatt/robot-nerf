63aaaa6652eff4499354b856390f57e0b7b53401
Matthew Ryan <drmatthewryan@gmail.com>
2021-03-28 21:03:56 +0100

    Tidying code

--- stepper/stepper.ino
+++ stepper/stepper.ino
@@ -11,7 +11,7 @@
 bool isHomed;
 int accelerateCount = 20;
 int rapidCount = 100;
-double dt = 200.0;//50.0; // microseconds
+double dt = 200.0;
 int desiredSteps = 0;
 
 int minv = 300;
@@ -22,16 +22,7 @@ float maxAngle = 113;
 
 float stepsPerDegree = 4 * 900.0 / 180.0; 
 
-
-
 double desiredPosition = 0;
-
-// simulation
-// double mass = 0.5;
-// double K = 60;
-// double d = 17;
-// double position = minAngle;
-// double speed = 0;
 
 double mass = 4;
 double K = 20;
@@ -77,131 +68,6 @@ void setup()
 
 }
 
-void update3() {
-  double a = 800;
-
-  double maxSpeed = 50;
-
-    if (!isDecelerating) {
-
-      if (desiredPosition>position) {
-        speed += a * (dt / 1000000);
-      } else { 
-        speed -= a * (dt / 1000000);
-      }
-
-        if (speed>maxSpeed)
-          speed = maxSpeed;
-        else if (speed<-maxSpeed)
-          speed = -maxSpeed;
-        
-        position += speed * (dt / 1000000);
-
-        float distanceToDecelerate = abs( 0.5*speed*speed/a );
-        float d = abs(desiredPosition-position);
-
-        if (distanceToDecelerate>=d)
-            isDecelerating = true;
-
-    } else {
-        // need to adjust this to make it stop in the right place
-        float d = abs( desiredPosition-position );
-        float dec = abs( 0.5*speed*speed / d );
-
-
-        if (desiredPosition >position) {
-            speed -= dec * (dt / 1000000);
-            if (speed<0)
-                speed = 0;
-        } else {
-            speed += dec * (dt / 1000000);
-            if (speed>0)
-              speed = 0;
-        }
-            
-        position += speed * (dt / 1000000);
-    }
-
-
-    int desiredSteps = angleToSteps( position );
-    
-    if (actualSteps<desiredSteps) {
-       digitalWrite( PIN_DIR_X, HIGH );
-     //  Serial.println("LOW");
-       actualSteps ++;
-       digitalWrite( PIN_STEP_X, HIGH );
-       delayMicroseconds( dt/2 );
-       digitalWrite( PIN_STEP_X, LOW );
-       delayMicroseconds( dt/2 );
-    } else if (actualSteps>desiredSteps) {
-       digitalWrite( PIN_DIR_X, LOW );
-      // Serial.println("HIGH");
-       actualSteps --;
-       digitalWrite( PIN_STEP_X, HIGH );
-       delayMicroseconds( dt/2 );
-       digitalWrite( PIN_STEP_X, LOW );
-       delayMicroseconds( dt/2 );
-    } else {
-      //Serial.println("---");
-       delayMicroseconds( dt );
-    }
-}
-
-void update2() {
-    double maxAcceleration = 10000;
-    double decceleration = 10000;
-    double deccelerateDistance = 5;
-    
-    double e = (desiredPosition - position);
-    
-    // calculate if it should deccelerate
-    double distance = abs(e);
-    if (distance < deccelerateDistance) {
-      Serial.println( "deccelerate" );
-      if (e>0) {
-        speed -= decceleration;
-        if (speed<0) speed =0;
-      } else {
-        speed += decceleration;
-        if (speed>0) speed =0;
-      }
-      
-    } else {
-
-  //     //speed factor
-  //     double f = 100;
-
-  //     double maxSpeed = 1000;
-
-  //     double desiredSpeed = e * f;
-
-  //     if (desiredSpeed>maxSpeed) desiredSpeed=maxSpeed;
-  //     if (desiredSpeed<-maxSpeed) desiredSpeed=-maxSpeed;
-      
-  //     Serial.print( "accelerate" );
-
-  // Serial.print( "e:" );
-  //     Serial.println( e);
-
-  //     Serial.print( " desiredSpeed:" );
-  //     Serial.println( desiredSpeed);
-
-  //     if (desiredSpeed>speed) {
-
-
-  //       speed += maxAcceleration;
-  //       if (speed>desiredSpeed)
-  //         speed = desiredSpeed;
-
-  //     } else if (desiredSpeed<speed)  {
-
-  //       speed -= maxAcceleration;
-  //       if (speed<desiredSpeed)
-  //         speed = desiredSpeed;
-
-  //     }
-   }
-}
 
 void update() {
     double f = K * (desiredPosition - position) - d*speed;
@@ -211,14 +77,10 @@ void update() {
     speed += a * (dt/1000000);
     position += speed * (dt/1000000);
 
-    // if (position<minAngle) position=minAngle;
-    // if (position>maxAngle) position=maxAngle;
-
     int desiredSteps = angleToSteps( position );
     
     if (actualSteps<desiredSteps) {
        digitalWrite( PIN_DIR_X, HIGH );
-     //  Serial.println("LOW");
        actualSteps ++;
        digitalWrite( PIN_STEP_X, HIGH );
        delayMicroseconds( dt/2 );
@@ -226,18 +88,15 @@ void update() {
        delayMicroseconds( dt/2 );
     } else if (actualSteps>desiredSteps) {
        digitalWrite( PIN_DIR_X, LOW );
-      // Serial.println("HIGH");
        actualSteps --;
        digitalWrite( PIN_STEP_X, HIGH );
        delayMicroseconds( dt/2 );
        digitalWrite( PIN_STEP_X, LOW );
        delayMicroseconds( dt/2 );
     } else {
-      //Serial.println("---");
        delayMicroseconds( dt );
     }
 }
-
 
 
 void loop()
@@ -261,21 +120,15 @@ void loop()
       Serial.println("GUN OFF");
       digitalWrite( PIN_GUN, LOW );
     } else {
-        int n = s1.toInt();
-       // desiredSteps = n;
-        desiredPosition = (double)n;
-        if (desiredPosition<minAngle) desiredPosition=minAngle;
-        if (desiredPosition>maxAngle) desiredPosition=maxAngle;
+      int n = s1.toInt();
+      desiredPosition = (double)n;
+      
+      if (desiredPosition<minAngle) desiredPosition=minAngle;
+      if (desiredPosition>maxAngle) desiredPosition=maxAngle;
     
       Serial.print( "Desired:" );
       Serial.println( desiredPosition );
-       
-      // int desiredSteps = angleToSteps( desiredPosition );
     
-      // Serial.print( "Desired:" );
-      // Serial.println( desiredSteps );
-    
-       isDecelerating = false;
     }
 
   }
@@ -287,7 +140,6 @@ void loop()
        if (home == HIGH) {
          isHomed = true;
          Serial.println("HOME");
-         //Serial.println( (home == LOW)?"LOW":"HIGH" );
          return;
        } 
       
